b3920027c790396f3b6ad293cb819e326ffd74b6
nimrariqat62-pixel <nimrariqat62@gmail.com>
2026-08-03 16:16:27 +0500

    Create Array_Input_Output.cpp

---
+++ Array_Input_Output.cpp
@@ -0,0 +1,27 @@
+
+
+# include <iostream>
+using namespace std;
+int main (){
+	
+	int numbers [5];
+	
+	cout << " enter first number : ";
+	cin >> numbers[0];
+	cout << " enter 2nd number : ";
+	cin  >> numbers[1];
+	cout << " enter 3rd number  : ";
+	cin >> numbers[2];
+	cout << " enter 4th number : ";
+	cin >> numbers[3];
+	cout << "  enter 5th number : ";
+	cin >> numbers[4];
+	
+	cout << " 0 :" << numbers[0] << endl;
+	cout << " 1 :" <<  numbers[1] << endl;
+	cout << " 2 :" <<  numbers[2] << endl;
+	cout << " 3 : " << numbers [3] << endl;
+	cout << " 4 : " << numbers[4] << endl;
+	
+	return 0;
+}
