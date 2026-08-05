966a7b6169a1f84c3c41ed790a525b2885eda158
nimrariqat62-pixel <nimrariqat62@gmail.com>
2026-08-05 16:58:03 +0500

    Update Array_Input_Output.cpp

--- Array_Input_Output.cpp
+++ Array_Input_Output.cpp
@@ -1,27 +1,28 @@
 
 
+
 # include <iostream>
 using namespace std;
 int main (){
 	
-	int numbers [5];
+	int numbers[6];
 	
 	cout << " enter first number : ";
 	cin >> numbers[0];
 	cout << " enter 2nd number : ";
-	cin  >> numbers[1];
-	cout << " enter 3rd number  : ";
+	cin >> numbers[1];
+	cout << " enter 3rd number : ";
 	cin >> numbers[2];
-	cout << " enter 4th number : ";
+	cout << " enter  4th number : ";
 	cin >> numbers[3];
-	cout << "  enter 5th number : ";
+	cout << " enter 5th number : ";
 	cin >> numbers[4];
+	cout << " enter 6th number : ";
+	cin >> numbers[5];
 	
-	cout << " 0 :" << numbers[0] << endl;
-	cout << " 1 :" <<  numbers[1] << endl;
-	cout << " 2 :" <<  numbers[2] << endl;
-	cout << " 3 : " << numbers [3] << endl;
-	cout << " 4 : " << numbers[4] << endl;
+	for ( int i=0 ;i<6; i++)
 	
+	cout << " i :  " << numbers[i] << endl;
 	return 0;
 }
+	
