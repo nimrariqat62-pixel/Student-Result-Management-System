52afb138016056a7e2ffe3dcb65ec22d1b903d93
nimrariqat62-pixel <nimrariqat62@gmail.com>
2026-08-27 14:21:10 +0500

    Create constructor.cpp

---
+++ constructor.cpp
@@ -0,0 +1,27 @@
+
+
+#include <iostream>
+using namespace std;
+class student {
+public:
+
+string name;
+int age;
+ student() {
+   
+   name = "nimra";
+   age = 18  ;
+      } 
+};
+
+int main(){
+        student s1;
+        
+        cout << s1.name << endl;
+        cout << s1. age << endl;
+        
+        return 0;
+}
+        
+  
+  
