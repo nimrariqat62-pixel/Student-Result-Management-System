79e0295875143a14d77109b8cfbc52ec81866b33
nimrariqat62-pixel <nimrariqat62@gmail.com>
2026-08-31 11:25:02 +0500

    Create single_inheritance.cpp

---
+++ single_inheritance.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+class child {
+	public:
+	 void showchild(){
+	 	cout <<"this is child class" << endl;
+	 }
+};
+int main(){
+	child c;
+	c.showchild();
+	return 0;
+}
